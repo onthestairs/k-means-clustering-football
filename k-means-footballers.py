e55e1c5aa481b59f623ceb42f886de5ee3120f90
Austin Platt <austinplatt@gmail.com>
2012-11-04 15:07:07 +0000

    tidied up a bit more. now does several kmeans clusters and choooses the best. hopefully the chances of getting stuck in a local min will be reduced

--- k-means-footballers.py
+++ k-means-footballers.py
@@ -4,11 +4,12 @@ import random
 all_stats = ("Date","Player ID","Player Surname","Player Forename","Team","Team Id","Opposition","Opposition id","Venue","Position Id","Appearances","Time Played","Starts","Substitute On","Substitute Off","Goals","First Goal","Winning Goal","Shots On Target inc goals","Shots Off Target inc woodwork","Blocked Shots","Penalties Taken","Penalty Goals","Penalties Saved","Penalties Off Target","Penalties Not Scored","Direct Free-kick Goals","Direct Free-kick On Target","Direct Free-kick Off Target","Blocked Direct Free-kick","Goals from Inside Box","Shots On from Inside Box","Shots Off from Inside Box","Blocked Shots from Inside Box","Goals from Outside Box","Shots On Target Outside Box","Shots Off Target Outside Box","Blocked Shots Outside Box","Headed Goals","Headed Shots On Target","Headed Shots Off Target","Headed Blocked Shots","Left Foot Goals","Left Foot Shots On Target","Left Foot Shots Off Target","Left Foot Blocked Shots","Right Foot Goals","Right Foot Shots On Target","Right Foot Shots Off Target","Right Foot Blocked Shots","Other Goals","Other Shots On Target","Other Shots Off Target","Other Blocked Shots","Shots Cleared off Line","Shots Cleared off Line Inside Area","Shots Cleared off Line Outside Area","Goals Open Play","Goals from Corners","Goals from Throws","Goals from Direct Free Kick","Goals from Set Play","Goals from penalties","Attempts Open Play on target","Attempts from Corners on target","Attempts from Throws on target","Attempts from Direct Free Kick on target","Attempts from Set Play on target","Attempts from Penalties on target","Attempts Open Play off target","Attempts from Corners off target","Attempts from Throws off target","Attempts from Direct Free Kick off target","Attempts from Set Play off target","Attempts from Penalties off target","Goals as a substitute","Total Successful Passes All","Total Unsuccessful Passes All","Assists","Key Passes","Total Successful Passes Excl Crosses Corners","Total Unsuccessful Passes Excl Crosses Corners","Successful Passes Own Half","Unsuccessful Passes Own Half","Successful Passes Opposition Half","Unsuccessful Passes Opposition Half","Successful Passes Defensive third","Unsuccessful Passes Defensive third","Successful Passes Middle third","Unsuccessful Passes Middle third","Successful Passes Final third","Unsuccessful Passes Final third","Successful Short Passes","Unsuccessful Short Passes","Successful Long Passes","Unsuccessful Long Passes","Successful Flick-Ons","Unsuccessful Flick-Ons","Successful Crosses Corners","Unsuccessful Crosses Corners","Corners Taken incl short corners","Corners Conceded","Successful Corners into Box","Unsuccessful Corners into Box","Short Corners","Throw Ins to Own Player","Throw Ins to Opposition Player","Successful Dribbles","Unsuccessful Dribbles","Successful Crosses Corners Left","Unsuccessful Crosses Corners Left","Successful Crosses Left","Unsuccessful Crosses Left","Successful Corners Left","Unsuccessful Corners Left","Successful Crosses Corners Right","Unsuccessful Crosses Corners Right","Successful Crosses Right","Unsuccessful Crosses Right","Successful Corners Right","Unsuccessful Corners Right","Successful Long Balls","Unsuccessful Long Balls","Successful Lay-Offs","Unsuccessful Lay-Offs","Through Ball","Successful Crosses Corners in the air","Unsuccessful Crosses Corners in the air","Successful crosses in the air","Unsuccessful crosses in the air","Successful open play crosses","Unsuccessful open play crosses","Touches","Goal Assist Corner","Goal Assist Free Kick","Goal Assist Throw In","Goal Assist Goal Kick","Goal Assist Set Piece","Key Corner","Key Free Kick","Key Throw In","Key Goal Kick","Key Set Pieces","Duels won","Duels lost","Aerial Duels won","Aerial Duels lost","Ground Duels won","Ground Duels lost","Tackles Won","Tackles Lost","Last Man Tackle","Total Clearances","Headed Clearances","Other Clearances","Clearances Off the Line","Blocks","Interceptions","Recoveries","Total Fouls Conceded","Fouls Conceded exc handballs pens","Total Fouls Won","Fouls Won in Danger Area inc pens","Fouls Won not in danger area","Foul Won Penalty","Handballs Conceded","Penalties Conceded","Offsides","Yellow Cards","Red Cards","Goals Conceded","Goals Conceded Inside Box","Goals Conceded Outside Box","Saves Made","Saves Made from Inside Box","Saves Made from Outside Box","Saves from Penalty","Catches","Punches","Drops","Crosses not Claimed","GK Distribution","GK Successful Distribution","GK Unsuccessful Distribution","Clean Sheets","Team Clean sheet","Error leading to Goal","Error leading to Attempt","Challenge Lost","Shots On Conceded","Shots On Conceded Inside Box","Shots On Conceded Outside Box","Team Formation","Position in Formation","Turnovers","Dispossessed","Big Chances","Big Chances Faced","Pass Forward","Pass Backward","Pass Left","Pass Right","Unsuccessful Ball Touch","Successful Ball Touch","Take-Ons Overrun","CompId","SeasId","Touches open play final third","Touches open play opp box","Touches open play opp six yards")
 meta_stats = ("Date","Player ID","Player Surname","Player Forename","Team","Team Id","Opposition","Opposition id","Venue","Position Id","Appearances","Time Played","Starts","Substitute On","Substitute Off","CompId","SeasId")
 keeper_stats = ("Turnovers","Goal Assist Goal Kick","Team Clean sheet","Team Formation","Position in Formation","Goals Conceded Outside Box", "Shots On Conceded","Goals Conceded Inside Box","Crosses not Claimed","Saves Made from Outside Box","Shots On Conceded Inside Box","Saves Made from Inside Box","Shots On Conceded Outside Box","Saves from Penalty","Big Chances Faced","Drops","Key Goal Kick","Goals Conceded","Saves Made","Catches","GK Distribution","GK Successful Distribution","GK Unsuccessful Distribution","Penalties Saved","Goal Assist Goal Kick")
-corner_stats = ("Successful Crosses Corners","Unsuccessful Crosses Corners","Corners Taken incl short corners","Corners Conceded","Successful Corners into Box","Unsuccessful Corners into Box","Short Corners","Successful Dribbles","Unsuccessful Dribbles","Successful Crosses Corners Left","Unsuccessful Crosses Corners Left","Successful Crosses Left","Unsuccessful Crosses Left","Successful Corners Left","Unsuccessful Corners Left","Successful Crosses Corners Right","Unsuccessful Crosses Corners Right","Successful Crosses Right","Unsuccessful Crosses Right","Successful Corners Right","Unsuccessful Corners Right","Successful Crosses Corners in the air","Unsuccessful Crosses Corners in the air","Key Corner")
+corner_stats = ("Successful Crosses Corners","Unsuccessful Crosses Corners","Corners Taken incl short corners","Corners Conceded","Successful Corners into Box","Unsuccessful Corners into Box","Short Corners","Successful Dribbles","Unsuccessful Dribbles","Successful Crosses Corners Left","Unsuccessful Crosses Corners Left","Successful Crosses Left","Unsuccessful Crosses Left","Successful Corners Left","Unsuccessful Corners Left","Successful Crosses Corners Right","Unsuccessful Crosses Corners Right","Successful Crosses Right","Unsuccessful Crosses Right","Successful Corners Right","Unsuccessful Corners Right","Successful Crosses Corners in the air","Unsuccessful Crosses Corners in the air","Key Corner","Goal Assist Corner")
 foot_specific_stats = ("Left Foot Goals","Left Foot Shots On Target","Left Foot Shots Off Target","Left Foot Blocked Shots","Right Foot Goals","Right Foot Shots On Target","Right Foot Shots Off Target","Right Foot Blocked Shots")
-penalty_stats = ("Penalties Taken","Penalty Goals","Penalties Saved","Penalties Off Target","Penalties Not Scored")
-set_piece_stats = ("Direct Free-kick Goals","Direct Free-kick On Target","Direct Free-kick Off Target","Blocked Direct Free-kick")
-disallowed_stats = meta_stats + keeper_stats + corner_stats + foot_specific_stats + penalty_stats + set_piece_stats
+penalty_stats = ("Penalties Taken","Penalty Goals","Penalties Saved","Penalties Off Target","Penalties Not Scored","Attempts from Penalties on target","Goals from penalties")
+set_piece_stats = ("Direct Free-kick Goals","Direct Free-kick On Target","Direct Free-kick Off Target","Blocked Direct Free-kick", "Key Set Pieces","Key Free Kick","Goal Assist Free Kick","Goal Assist Set Piece")
+throw_in_stats = ("Throw Ins to Own Player","Throw Ins to Opposition Player")
+disallowed_stats = meta_stats + keeper_stats + corner_stats + foot_specific_stats + penalty_stats + set_piece_stats + throw_in_stats
 allowed_stats = tuple(set(all_stats)-set(disallowed_stats))
 
 print "ALLOWED STATS:"
@@ -36,8 +37,8 @@ class Player():
 		average_stats = {k: (float(v)/total_games) for k,v in total_stats.items()}
 		self.stats = average_stats
 
-	def calculate_normalised_stats(self, max_stats):
-		self.normalised_stats = {k: (v/max_stats[k]) for k,v in self.stats.items() if max_stats[k] != 0}
+	def calculate_normalised_stats(self, max_stats, min_stats):
+		self.normalised_stats = {k: (v-min_stats[k])/(max_stats[k]-min_stats[k]) for k,v in self.stats.items() if max_stats[k] != 0}
 
 def parse(file='stats.csv', allowed = lambda x: True):
 	players = {}
@@ -62,30 +63,40 @@ def parse(file='stats.csv', allowed = lambda x: True):
 	return player_list
 
 def get_max_stats(players):
-	max_stats = {k:0 for k in players.pop().stats.keys()}
+	max_stats = {k:0 for k in players[0].stats.keys()}
 	for p in players:
 		for k,v in p.stats.items():
 			if v > max_stats[k]:
 				max_stats[k] = v
 	return max_stats
 
+def get_min_stats(players):
+	min_stats = {k:0 for k in players[0].stats.keys()}
+	for p in players:
+		for k,v in p.stats.items():
+			if v < min_stats[k]:
+				min_stats[k] = v
+	return min_stats
+
 class kmeans():
 
-	def __init__(self, N, players, distance_function):
+	def __init__(self, N, players, distance_function, threshold=0.01):
 		self.N = N
 		self.players = players
 		self.distance_function = distance_function
 		self.features = players[0].stats.keys()
+		self.threshold = threshold
+		self.calculate()
 
 	def init_means(self):
 		self.means = [p.normalised_stats for p in random.sample(self.players, self.N)]
 
-	def calculate(self, threshold=0.01):
+	def calculate(self):
 		self.init_means()
 		old_total_distance = 10000
-		new_total_distance = old_total_distance - threshold - 10
+		new_total_distance = old_total_distance - self.threshold - 10
 		count = 0
-		while (old_total_distance - new_total_distance) > threshold:
+		while (old_total_distance - new_total_distance) > self.threshold:
 			total = 0
 			self.clusters = [[] for m in self.means]
 			for p in self.players:
@@ -101,8 +112,9 @@ class kmeans():
 				total += smallest
 			old_total_distance, new_total_distance = new_total_distance, total
 			count = count + 1
-			print "Done iteration {0}. New distance is {1}. Old distance is {2}.".format(count, new_total_distance, old_total_distance)
+			print "Done iteration {0}. New distance is {1}.".format(count, new_total_distance, old_total_distance)
 			self.recalculate_means()
+		self.final_error = new_total_distance
 		self.mean_features()
 
 	def recalculate_means(self):
@@ -148,31 +160,33 @@ only_midfielders = disjunct([only_2_midfielders,only_3_midfielders])
 no_keepers_no_substitutes = conjunct([no_keepers, no_substitutes])
 
 
-players = parse(allowed=no_keepers)
+players = parse(allowed=only_midfielders)
 print "parsed"
 
 a = len(players)
-players = [p for p in players if p.games_played > 15]
+players = [p for p in players if p.games_played > 10]
 b = len(players)
 
 print "Got rid of {0} players.".format(a-b)
 
 max_stats = get_max_stats(players)
-print "got max stats"
+min_stats = get_min_stats(players)
+print "got max, min stats"
 
 print "poor stats"
 print [k for k,v in max_stats.items() if v == 0]
 
 for p in players:
-	p.calculate_normalised_stats(max_stats)
+	p.calculate_normalised_stats(max_stats, min_stats)
 print "normalised the stats"
 
 
 distance = lambda x,y: sum([(x[k]-y[k])**2 for k in list( set( x.keys() ).intersection( set(y.keys()) ) )])
 
-kmeans = kmeans(10, players, distance)
-kmeans.calculate()
-print "calculated clusters"
+ks = [kmeans(3,players,distance) for i in range (5)]
+ks = sorted(ks, key= lambda x: x.final_error)
+
+kmeans = ks[0]
 
 def p_c():
 	print "+-------+"
